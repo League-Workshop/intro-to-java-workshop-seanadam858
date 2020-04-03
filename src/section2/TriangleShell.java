a2600cf7b43a4db48d05cb9910e2fcc923e35394
root <root@6ee69d1e1390>
2020-04-03 21:47:07 +0000

    Day 5

--- src/section2/TriangleShell.java
+++ src/section2/TriangleShell.java
@@ -5,30 +5,36 @@ import org.jointheleague.graphical.robot.Robot;
 public class TriangleShell {
 
 	// 1. Create a new Robot
-
+	Robot rob=new Robot("batman");
 	
 	void go() {
 		drawTriangle(100); // 3. delete this line (used only for testing)
 
 		// 6. Make the robot go as fast as possible
-
+rob.setSpeed(100);
 		// 4. make a variable to hold the length of the triangle and set it to 50
-
+int tri=50;
 		// 7. Use a for loop to repeat steps #9 to #10, 60 times
-
+for(int i=0;i<60;i++) {
 			// 9. Change the color of the pen to a random color
-	
+	rob.setRandomPenColor();
 			// 8. Increase the length variable by 10
-	
+tri=tri+10;
 			// 5. call your drawTriangle() method using your length variable
-	
+	drawTriangle(tri);
 			// 10. Turn the robot 6 degrees to the right
-
-	}
+rob.turn(6);
+}
+}
+	
 
 	/* 2. fill in the method below to draw a triangle. Use the length variable when you call move(). */
 	private void drawTriangle(int length) {
-		
+		rob.setX(450);
+		rob.setY(350);
+		rob.penDown();
+		rob.move(150);
+		rob.turn(60);
 		
 		
 	}
